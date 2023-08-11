f8c98c6d2a6a5b75358f114528c8adbaa3b3bdc4
marciafoster <mfoster9tre@gmail.com>
2023-08-11 12:04:52 -0400

    React App Started; Boiler-Plate Code removed; Installed React Router v6

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+import { BrowserRouter as Router } from "react-router-dom"
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <Router>
+     
+    <h1>Screen Views</h1>
+    
+    </Router>  
+    </>
+  )
+}
+
+export default App
